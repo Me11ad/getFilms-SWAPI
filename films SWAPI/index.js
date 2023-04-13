eff5696859d7af0812190ea98871a9d3cc2d58f6
Me11ad <112916199+Me11ad@users.noreply.github.com>
2023-04-13 11:47:05 +0500

    Add files via upload

--- films SWAPI/index.js
+++ films SWAPI/index.js
@@ -1,10 +1,14 @@
-async function getFilms(id) {
+async function getFilms() {
     try {
-        const filmResponse = await fetch(`https://swapi.dev/api/films/${id}/`);
+        const filmResponse = await fetch(`https://swapi.dev/api/films/`);
         const film = await filmResponse.json();
+
+        const movieUrl = film.films;
+        const movieResponse = await fetch(`https://${movieUrl}`);
+        return movieResponse.json();
     } catch (err) {
         console.error('Произошла ошибка!', err);
     }
 }
 
-getFilms(1).then((profile) => {console.log(profile)}) 
+getFilms(1).then((profile) => {console.log(profile)});
